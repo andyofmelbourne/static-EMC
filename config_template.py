a8a4e559c6a4ac6e57f0e2ec092e21c26369f031
Andrew James Morgan <andyofmelbourne@gmail.com>
2024-02-23 11:39:15 +1100

    added imshow lambda function to config for 2d display of images

--- config_template.py
+++ config_template.py
@@ -10,8 +10,9 @@ classes            = 200
 background_classes = 1
 max_frames         = 5000
 frame_shape        = (16, 128, 512)
+imshow             = lambda x: x[0, :, :128]
 # just use the first part of the first panel (low q)
-pixels             = np.arange(1024**2).reshape(frame_shape)[0, :, :128]
+pixels             = imshow(np.arange(1024**2).reshape(frame_shape))
 filter_by          = 'static_emc/good_hit' # or None
 
 tol_P = 1e-2
